1fc4c3e3230f8a11a8a83651839d236e47812026
dsk <ex-dingshikun001@pingan.com.cn>
2018-08-17 17:24:39 +0800

    writetotxt

--- listxf.py
+++ listxf.py
@@ -2,31 +2,34 @@ from lxml import etree
 import requests
 import time
 
-url = 'http://xf.m.pinganfang.com/sh'
-# requests.get()返回的是URL的响应码200，
-# 加上.text，下载的是网页的源码
-data = requests.get(url,verify=False).text
+with open('list.txt','w',encoding='utf-8') as f:                     
+	url = 'http://xf.m.pinganfang.com/sh'
+	# requests.get()返回的是URL的响应码200，
+	# 加上.text，下载的是网页的源码
+	data = requests.get(url,verify=False).text
 
-# 使用etree.HTML()方法解析data数据
-# s = etree.HTML(data)
-# xfname = s.xpath('//*[@id="listWrap"]/ul/li/a/div[2]/div/h3/em/text()')
-# for name in xfname:
-# 	time.sleep(0.2)
-# 	print(name)
-count = 0
-s = etree.HTML(data)
-file = s.xpath('//*[@id="listWrap"]/ul/li/a/div[2]/div')
-for i in file: 
-	name = i.xpath('./h3/em/text()')[count]
-	price = i.xpath('div[3]/text()')[count]
-	adds = i.xpath('div[1]/text()')[count].strip('\\')
-	# count += 1
-	time.sleep(0.2)
-	# print(name)
-	# print("%s,%s"%(name,adds))
-	print('{} -- {} -- {}'.format(name,price,adds))
-# print(count)
-
+	# 使用etree.HTML()方法解析data数据
+	# s = etree.HTML(data)
+	# xfname = s.xpath('//*[@id="listWrap"]/ul/li/a/div[2]/div/h3/em/text()')
+	# for name in xfname:
+	# 	time.sleep(0.2)
+	# 	print(name)
+	count = 0
+	s = etree.HTML(data)
+	file = s.xpath('//*[@id="listWrap"]/ul/li/a/div[2]/div')
+	for i in file: 
+		name = i.xpath('./h3/em/text()')[count]
+		price = i.xpath('div[3]/text()')[count]
+		adds = i.xpath('div[1]/text()')[count].strip('\\')
+		# count += 1
+		time.sleep(0.2)
+		f.write('{} -- {} -- {}\n'.format(name,price,adds) )
+		# print(name)
+		# print("%s,%s"%(name,adds))
+		# print('{} -- {} -- {}'.format(name,price,adds))
+	# print(count)
+# //*[@id="J_maidian"]/div[1]/div[2]/div/div[3]/div/span
+# //*[@id="J_maidian"]/div[1]/div[2]/div/div[1]/div/h2/a
 # //*[@id="listWrap"]/ul/li[2]/a/div[2]/div/div[3]
 # //*[@id="listWrap"]/ul/li[4]/a/div[2]/div/h3/em
 # //*[@id="listWrap"]/ul/li[4]/a/div[2]/div/div[1]
